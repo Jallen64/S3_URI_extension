c4c3e0762769dd06b3edab853266ceaaff013234
Joshua Allen <jaallenusa@gmail.com>
2021-05-20 12:37:02 -0500

    Fixed String Concat
    didnt work before

--- js/tools.js
+++ js/tools.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 
 	$('#clearbutton').click(function() {
 		var chr = $('#text-normal').val();
-		var head = "s3.console.aws.com/s3/buckets/"
+		var head = "s3.console.aws.amazon.com/s3/buckets/"
 		var url = head.concat(chr.slice(5)).toString()
 		$('#text-convert').val(url)
 	});
